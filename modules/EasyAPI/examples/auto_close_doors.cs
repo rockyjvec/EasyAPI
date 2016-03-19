93f625a07721b169ef7390a9229f1a3cfdc49d23
rockyjvec <github@thecardwells.net>
2016-03-18 19:26:07 -0600

    Updated auto close door example to use new event code.

--- modules/EasyAPI/examples/auto_close_doors.cs
+++ modules/EasyAPI/examples/auto_close_doors.cs
@@ -7,41 +7,20 @@ public class Example : EasyAPI
         doors = Blocks.OfType("Sliding Door");
 
         // split work over time so it doesn't throw a complexity error (tested with 384 doors)
-        In(1 * EasyAPI.Seconds, delegate() {
-              doors.AddEvent(  
-                  delegate(EasyBlock block) {
-                      return block.Open();  
-                  },  
-                  doorOpened  
-              );  
+        In(1 * EasyAPI.Seconds, delegate() { // In one second, create the events.
+            doors.AddEvent(  
+                delegate(EasyBlock block) { // When a door is opened
+                    return block.Open();  
+                },  
+                delegate(EasyBlock block) { // Do the following
+                    In(2 * EasyAPI.Seconds, delegate() {  // In 2 seconds
+                        block.ApplyAction("Open_Off"); // close the door
+                    });                    
+                    
+                    return true;
+                },
+                true // only trigger event when the condition (door open) goes from false to true
+            );
         });
-    }  
-  
-    public bool doorOpened(EasyBlock door)  
-    {
-        In(2 * EasyAPI.Seconds, delegate() {
-            door.ApplyAction("Open_Off");
-        });
- 
-        door.AddEvent(  
-            delegate(EasyBlock block) {  
-                return !block.Open();  
-            },  
-            doorClosed  
-        );  
-  
-        return false;  
-    }  
-  
-    public bool doorClosed(EasyBlock door)  
-    {  
-        door.AddEvent(  
-            delegate(EasyBlock block) {  
-                return block.Open();  
-            },  
-            doorOpened  
-        );  
-  
-        return false;  
     }  
 }  
